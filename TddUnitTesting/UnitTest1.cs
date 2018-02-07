1484a89179ef033c500d6ce4b1f748cb08dddb16
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-07 21:08:23 +0000

    Add project files.

---
+++ TddUnitTesting/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestClassLibrary; 
+namespace TddUnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddNumberTest()
+        {
+            double ActualOutput;
+            Class1 NewInstance = new Class1();
+
+            ActualOutput = NewInstance.AddNumbers(3.68, 6.88);
+            Assert.IsTrue(ActualOutput <= 10); 
+        }
+
+        [TestMethod]
+        public void StringTest()
+        {
+            string sActualOutput;
+            string sName = "Bob";
+            string sExpected = "Hello, Welcome " + sName + "!";
+
+            Class1 NewInstance = new Class1();
+            sActualOutput = NewInstance.Greeting(sName);
+
+            Assert.IsTrue(sActualOutput.StartsWith("Hello, Welcome"));
+            Assert.IsTrue(sActualOutput.Contains(sName));
+            Assert.IsTrue(sActualOutput.EndsWith("!"));
+            Assert.AreEqual(sExpected, sActualOutput); 
+
+            
+        }
+
+        [TestMethod]
+        public void HolidayTest()
+        {
+            {
+                bool ActualOutput;
+                //DateTime inputDate;
+                
+               // string sExpected = "01-Jan-2014";                 
+             
+                Class1 NewInstance = new Class1();
+                ActualOutput = NewInstance.checkBankHoliday(inputDate);
+                // New Years Day
+                if (inputDate == Convert.ToDateTime("01-Jan-2014"))
+                {
+                    ActualOutput = true; 
+                }
+               
+               /* Assert.IsTrue(ActualOutput.StartsWith("01"));
+                Assert.IsTrue(ActualOutput.Contains("01"));
+                Assert.IsTrue(ActualOutput.EndsWith("2014"));
+                Assert.AreEqual(Convert.ToDateTime(sExpected), Convert.ToDateTime(ActualOutput));*/ 
+            }
+
+
+
+
+        }
+
+
+
+        public DateTime inputDate { get; set; }
+    }
+}
---
+++ TestClassLibrary/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClassLibrary; 
+
+namespace TestClassLibrary
+{
+    public class Class1
+    {
+        public double AddNumbers(double valueA, double valueB)
+        {
+            double Output = valueA + valueB;
+            if (Output > 10)
+            {
+                Output = 10; 
+            }
+            return Output; 
+        }
+
+        public string Greeting(string sName)
+            {
+                string sOutput = "Hello, Welcome " + sName + "!";
+                return sOutput; 
+            }
+
+
+        // Method for Bank Holiday output.
+
+        public bool checkBankHoliday(DateTime inputDate)
+        {
+            bool validBankHoliday = false;
+
+            // New Years Day
+
+            if ((inputDate.Day == 01) && (inputDate.Month == 01) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = true;
+            }
+
+            // Valentines Day
+
+            else if ((inputDate.Day == 14) && (inputDate.Month == 02) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = false;
+            }
+
+            // St Patricks Day
+
+            else if ((inputDate.Day == 17) && (inputDate.Month == 03) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = false;
+            }
+
+            // April Fools Day
+
+            else if ((inputDate.Day == 01) && (inputDate.Month == 04) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = false;
+            }
+
+            // St Georges Day
+
+            else if ((inputDate.Day == 23) && (inputDate.Month == 04) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = false;
+            }
+
+            // Halloween
+
+            else if ((inputDate.Day == 31) && (inputDate.Month == 10) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = false;
+            }
+
+            // Bonfire Night
+
+            else if ((inputDate.Day == 05) && (inputDate.Month == 11) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = false;
+            }
+
+            // Christmas Day
+
+            else if ((inputDate.Day == 25) && (inputDate.Month == 12) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = true;
+            }
+
+            // Boxing Day
+
+            else if ((inputDate.Day == 26) && (inputDate.Month == 12) && (inputDate.Year == 2014))
+            {
+                validBankHoliday = true;
+            }
+
+            return validBankHoliday;
+
+
+        }
+
+
+
+     }
+}
